a46b5d40e8b04df27d5f506d7ae66b52c7d90fa8
Carlos Tojal <carlos.tojal@hotmail.com>
2020-01-19 19:13:33 +0000

    Now Barnaby can get weather of specific location.

--- BarnabyTools.py
+++ BarnabyTools.py
@@ -44,22 +44,47 @@ class BarnabyTools:
     def weather(self, sentence):
         for word in sentence.split():
             if word.lower() in WEATHER_INPUTS:
-                URL = "https://api.ipgeolocation.io/ipgeo"
-                PARAMS = {
-                    'apiKey': '747a7ad91fc84b2b83dff71d9ac0af16',
-                }
-                r = requests.get(url = URL, params = PARAMS)
-                city = r.json()['city']
-
+                city_in_sentence = False
+                # searches city in user sentence
                 URL = "https://api.openweathermap.org/data/2.5/weather"
-                PARAMS = {
-                    'apikey': 'c9f1d2a6eaec5f917473c187547ba288',
-                    'q': city,
-                    'units': 'metric'
-                }
-                r = requests.get(url = URL, params = PARAMS)
-                data = r.json()
-                return "It's " + str(data['main']['temp']) + "ºC, " + data['weather'][0]['description'] + " in " + data['name'] + " (I think it's your location)."
+                for city_search in sentence.split():
+                    PARAMS = {
+                        'apikey': 'c9f1d2a6eaec5f917473c187547ba288',
+                        'q': city_search,
+                        'units': 'metric'
+                    }
+                    r = requests.get(url = URL, params = PARAMS)
+                    data = r.json()
+                    if data['cod'] == 200:
+                        city = data['name']
+                        city_in_sentence = True
+                        break
+                if not city_in_sentence:
+                    # gets user city by IP address
+                    URL = "https://api.ipgeolocation.io/ipgeo"
+                    PARAMS = {
+                        'apiKey': '747a7ad91fc84b2b83dff71d9ac0af16',
+                    }
+                    r = requests.get(url = URL, params = PARAMS)
+                    city = r.json()['city']
+
+                    URL = "https://api.openweathermap.org/data/2.5/weather"
+                    for city_search in sentence.split():
+                        PARAMS = {
+                            'apikey': 'c9f1d2a6eaec5f917473c187547ba288',
+                            'q': city,
+                            'units': 'metric'
+                        }
+                        r = requests.get(url = URL, params = PARAMS)
+                        data = r.json()
+
+                # returns response to user
+                output = "It's " + str(data['main']['temp']) + "ºC, " + data['weather'][0]['description'] + " in " + data['name']
+                if not city_in_sentence:
+                    output += " (I think it's your location)."
+                else:
+                    output += "."
+                return output
 
     def LemTokens(self, tokens):
         return [self.lemmer.lemmatize(token) for token in tokens]
@@ -75,11 +100,9 @@ class BarnabyTools:
         tfid = TfidfVec.fit_transform(self.sent_tokens)
         vals = cosine_similarity(tfid[-1], tfid)
         idx = vals.argsort()[0][-2]
-        # idx = vals.argsort()[0][0]
         flat = vals.flatten()
         flat.sort()
         req_tfid = flat[-2]
-        # req_tfid = flat[0]
 
         if(req_tfid==0):
             bot_response = bot_response + "I'm sorry. I don't understand."
